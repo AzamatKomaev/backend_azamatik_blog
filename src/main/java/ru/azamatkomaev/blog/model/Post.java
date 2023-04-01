cdaed252bf810e11fd1789ef04e9116b87015803
Азамат Комаев <azamatkomaev15@gmail.com>
2023-04-02 00:31:26 +0300

    add Post entity and migration for it

---
+++ src/main/java/ru/azamatkomaev/blog/model/Post.java
@@ -0,0 +1,33 @@
+package ru.azamatkomaev.blog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+}
---
+++ src/main/resources/db/changelog/01__Create_posts_table.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset azamat_komaev:01
+create table posts (
+    id serial primary key not null,
+    title varchar(255) not null,
+    content text not null,
+    created_at timestamp not null default now()
+);
+--rollback drop table posts;
