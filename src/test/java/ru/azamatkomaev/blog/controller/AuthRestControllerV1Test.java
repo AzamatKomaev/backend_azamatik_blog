e354c9388c63a660b43bd3c7323e5157ada59212
Азамат Комаев <azamatkomaev15@gmail.com>
2023-04-03 20:09:36 +0300

    add method sources for password size and successful register user tests

--- src/test/java/ru/azamatkomaev/blog/controller/AuthRestControllerV1Test.java
+++ src/test/java/ru/azamatkomaev/blog/controller/AuthRestControllerV1Test.java
@@ -2,11 +2,13 @@ package ru.azamatkomaev.blog.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,11 +17,12 @@ import ru.azamatkomaev.blog.model.User;
 import ru.azamatkomaev.blog.request.RegisterRequest;
 import ru.azamatkomaev.blog.service.UserService;
 
+import java.util.stream.Stream;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,6 +45,20 @@ public class AuthRestControllerV1Test {
     private final String REGISTER_ENDPOINT = "/api/v1/auth/register";
     private final String LOGIN_ENDPOINT = "/api/v1/auth/login";
 
+    private static Stream<Arguments> providePasswordSizeLessThanEIGHTUserData() {
+        return Stream.of(
+            Arguments.of("Azamat1", "s"),
+            Arguments.of("Azamat2", "small"),
+            Arguments.of("Azamat3", "small_p")
+        );
+    }
+
+    private static Stream<Arguments> provideValidUserData() {
+        return Stream.of(
+            Arguments.of("Azamat", "azamat12345")
+        );
+    }
+
     @Test
     public void testRegisterWithEmptyBody() throws Exception {
         when(userService.saveUser(anyString(), anyString())).thenReturn(null);
@@ -57,13 +74,14 @@ public class AuthRestControllerV1Test {
         verify(userService, never()).saveUser(anyString(), anyString());
     }
 
-    @Test
-    public void testRegisterWithPasswordSizeLessThanEIGHT() throws Exception {
+    @ParameterizedTest
+    @MethodSource("providePasswordSizeLessThanEIGHTUserData")
+    public void testRegisterWithPasswordSizeLessThanEIGHT(String username, String password) throws Exception {
         when(userService.saveUser(anyString(), anyString())).thenReturn(null);
 
         RegisterRequest request = RegisterRequest.builder()
-            .username("Azamat")
-            .password("small") // length of the string less than 8
+            .username(username)
+            .password(password) // length of the string less than 8
             .build();
 
         RequestBuilder requestBuilder = post(REGISTER_ENDPOINT)
@@ -77,10 +95,9 @@ public class AuthRestControllerV1Test {
         verify(userService, never()).saveUser(anyString(), anyString());
     }
 
-    @Test
-    public void testSuccessfullyRegisterUser() throws Exception {
-        String username = "Azamat";
-        String password = "password12345";
+    @ParameterizedTest
+    @MethodSource("provideValidUserData")
+    public void testSuccessfullyRegisterUser(String username, String password) throws Exception {
         User user = User.builder()
             .id(1L)
             .username(username)
